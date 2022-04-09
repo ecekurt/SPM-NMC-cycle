7e70502990677b2b4f149fc1e4c9d4c957ab3748
ecekurt <56387994+ecekurt@users.noreply.github.com>
2022-04-09 18:15:45 +0300

    Update coolingcontrol.m

--- coolingcontrol.m
+++ coolingcontrol.m
@@ -1,30 +1,23 @@
-Tamb=[298 310];
+function coolingcontrol(T,cur,R_tot_n,p)
 temp_fan_on=308;
 temp_fan_off=303;
 
 fan_onoff_flag=0;
 
-for d=1:length(Tamb)
-temp(1)=Tamb(d);
-for k=1:length(power)
-    
-if  temp(k)>=temp_fan_on
+if  T>=temp_fan_on
     fan_onoff_flag=1;
     Qohmic =  cur.^2*R_tot_n;
     Qremv=p.h*p.A*(T(k)-p.T_amb);   
-%     temp_diff=(heat_gen-heat_rem)/(m_mod*cp_mod); %temperature change in module per each time step
-elseif temp(k)<temp_fan_on && temp(k)>=temp_fan_off && fan_onoff_flag_init==1
+
+elseif T<temp_fan_on && T>=temp_fan_off && fan_onoff_flag_init==1
     fan_onoff_flag=1;
-    [heat_gen]=heatgenerated(I,res);
-    [heat_rem]=air_cooling(temp_init);
-%     temp_diff=(heat_gen-heat_rem)/(m_mod*cp_mod); %temperature change in module per each time step
+    Qohmic =  cur.^2*R_tot_n;
+    Qremv=p.h*p.A*(T(k)-p.T_amb); 
+
 else
     fan_onoff_flag=0;
-    [heat_gen]=heatgenerated_ECM3(I,v_oc,vT);
-  heat_rem=0;
+    Qohmic =  cur.^2*R_tot_n;
+    heat_rem=0;
    
-   temp_diff=(heat_gen-heat_rem)/(m_mod*cp_mod*N_mod); %temperature change in module per each time step
-   temp(k+1)=temp(k)+temp_diff;
-end
-end
+
 end
