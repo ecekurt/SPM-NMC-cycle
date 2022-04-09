e90fbc954b32556ad33a7d4503626bc831494ca7
ecekurt <56387994+ecekurt@users.noreply.github.com>
2022-04-09 21:49:59 +0300

    Update coolingcontrol.m

--- coolingcontrol.m
+++ coolingcontrol.m
@@ -1,4 +1,4 @@
-function coolingcontrol(T,cur,R_tot_n,p)
+function [Qohmic,Qremv]=coolingcontrol(T,cur,R_tot_n,p)
 temp_fan_on=308;
 temp_fan_off=303;
 
@@ -7,17 +7,17 @@ fan_onoff_flag=0;
 if  T>=temp_fan_on
     fan_onoff_flag=1;
     Qohmic =  cur.^2*R_tot_n;
-    Qremv=p.h*p.A*(T(k)-p.T_amb);   
+    Qremv= p.h*p.A*(T-p.T_amb);   
 
-elseif T<temp_fan_on && T>=temp_fan_off && fan_onoff_flag_init==1
+elseif T<temp_fan_on && T>=temp_fan_off && fan_onoff_flag==1
     fan_onoff_flag=1;
     Qohmic =  cur.^2*R_tot_n;
-    Qremv=p.h*p.A*(T(k)-p.T_amb); 
+    Qremv=p.h*p.A*(T-p.T_amb); 
 
 else
     fan_onoff_flag=0;
     Qohmic =  cur.^2*R_tot_n;
-    heat_rem=0;
+    Qremv=0;
    
 
 end
