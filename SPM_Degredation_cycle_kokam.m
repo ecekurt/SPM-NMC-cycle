5f4d2251729c5ef27ae8883b61f8582d6ccf0265
ecekurt <56387994+ecekurt@users.noreply.github.com>
2022-06-16 10:30:04 +0300

    Update SPM_Degredation_cycle_kokam.m

--- SPM_Degredation_cycle_kokam.m
+++ SPM_Degredation_cycle_kokam.m
@@ -22,7 +22,7 @@ global OCVcell
 
 % Discharge only
  p.C_rate= 1;
- p.cycle_number=500;
+ p.cycle_number=5;
  p.Nc= p.cycle_number*2;
 
 tend= 3600*p.Nc/p.C_rate;
@@ -115,8 +115,8 @@ if mod(i,2)==1
     
     for k=1+eventtime(i):eventtime(i+1)
 
-    [~,theta_p(k),theta_n(k),V_spm(k),V_ocv(k), Ds_n(k), Ds_p(k), ...
-        k_n(k), k_p(k), sn(k), sp(k), eta_sei_n(k), Qohmic(k), Qreaction(k),Qremv(k),T_dot(k),Q_dot(k),sei_dot(k),R_tot_n(k),BAh(k),cur(k)]...
+    [~,theta_p(k),theta_n(k),V_spm(k),V_ocv(k), Uref_n(k), Uref_p(k), ...
+        cur(k)]...
         =ode_SPMT_discharge(t(k),x(k,:)');
     SOCp(k)=( theta_p(k)- p.theta_p_max )/( p.theta_p_min -p.theta_p_max);
     SOCn(k)=( theta_n(k)- p.theta_n_min )/( p.theta_n_max -p.theta_n_min);
@@ -129,8 +129,8 @@ else
         
     for k=1+eventtime(i):eventtime(i+1)
         
-    [~,theta_p(k),theta_n(k),V_spm(k),V_ocv(k), Ds_n(k), Ds_p(k), ...
-        k_n(k), k_p(k), sn(k), sp(k), eta_sei_n(k), Qohmic(k), Qreaction(k),Qremv(k),T_dot(k),Q_dot(k),sei_dot(k),R_tot_n(k),BAh(k),cur(k)]...
+    [~,theta_p(k),theta_n(k),V_spm(k),V_ocv(k), Uref_n(k), Uref_p(k), ...
+        cur(k)]...
         =ode_SPMT_charge(t(k),x(k,:)');
     SOCp(k)=( theta_p(k)- p.theta_p_max )/( p.theta_p_min -p.theta_p_max);
     SOCn(k)=( theta_n(k)- p.theta_n_min )/( p.theta_n_max -p.theta_n_min);
@@ -172,7 +172,6 @@ figure
 plot(cycle,seigrowth,'.-'); 
 xlabel('Cycle Number');
 ylabel('Sei thickness [m]');
-legend('FDM-SPM', 'SLIDE-SPM');
 grid on;
 
 
@@ -199,18 +198,18 @@ cur=data.Current(t);
 TEMP=T;
 %% Solid phase dynamics
 
-% Molar flux for solid phase
+% Molar flux for solid phase [mol m-2 s-1]
 J_p=-(cur./p.Area_p)./(p.Faraday*p.a_p*p.L_p);
-J_n=(cur./p.Area_n)./(p.Faraday*p.a_n*p.L_n);  % molar flux on the negative particle [mol m-2 s-1]
+J_n=(cur./p.Area_n)./(p.Faraday*p.a_n*p.L_n);  
 
-% Solid phase diffusivity temperature dependence
+% Solid phase diffusivity temperature dependence [m s-1]
 p.Ds_n = p.Ds_n0 * exp(p.E.Dsn/p.R*(1/p.T_ref - 1/TEMP));
 p.Ds_p = p.Ds_p0 * exp(p.E.Dsp/p.R*(1/p.T_ref - 1/TEMP)) ;
 
 % Matrices for solid-phase Li concentration
  [A_p,A_n,B_n,B_p]= matrixs(p);
 
-% Calculation of the surface concentration
+% Calculation of the surface concentration [mol m-3]
 
 c_ss_p= U_p(end);
 c_ss_n= U_n(end);
@@ -218,35 +217,20 @@ c_ss_n= U_n(end);
 
 %% Calculation of potential of the cell
 
-% SOC of the electrodes  
+% li-fraction  of the electrodes  
  [theta_p,theta_n]=getsoc(c_ss_p,c_ss_n,p);
  
-%% li-fraction
-
-AMp= (p.eps_s_p*p.L_p*p.Area_p);
-AMn= (p.eps_s_n*p.L_n*p.Area_n);
-BAh =cur*t/3600;
-sp = cur * t / (p.Faraday) / (p.eps_s_p*p.L_p*p.Area_p) / p.c_s_p_max;
-sn = cur * t / (p.Faraday) / (p.eps_s_n*p.L_n*p.Area_n) / p.c_s_n_max;
-
-%% cell OCV
-% cell_Ah=OCVcell(:,1);
-% cell_OCV=OCVcell(:,2);
-% V_ocv= interp1(cell_Ah,cell_OCV,Ah,'linear'); % Ah charge sırasında eksi oluyor!!!
-
-%% OCV 
+% OCV of the cell [v]
 
 [Uref_p, Uref_n]=refpotantial (theta_p, theta_n,KokamOCVNMC, KokamNMC, KokamOCVC, KokamC, OCVcell);
+V_ocv = Uref_p-Uref_n  ;
 
-% OCV of the cell
-  V_ocv = Uref_p-Uref_n  ;
-
-% Kinetic reaction rate, adjusted for Arrhenius temperature dependence
+% Kinetic reaction rate, adjusted for Arrhenius temperature dependence [(A/m^2)*(mol^3/mol)^(1+alpha)]
 
  p.k_n = p.k_n0 * exp(p.E.kn/p.R*(1/p.T_ref - 1/TEMP)); 
  p.k_p = p.k_p0 * exp(p.E.kp/p.R*(1/p.T_ref - 1/TEMP)); 
 
-% Exchange current density
+% Exchange current density [I m-2]
 
  i_0n = p.k_n * p.Faraday * sqrt(((p.c_s_n_max - c_ss_n) .* c_ss_n .* p.ce));
  i_0p = p.k_p * p.Faraday * sqrt(((p.c_s_p_max - c_ss_p) .* c_ss_p .* p.ce));
@@ -254,38 +238,37 @@ sn = cur * t / (p.Faraday) / (p.eps_s_n*p.L_n*p.Area_n) / p.c_s_n_max;
  xn= 0.5 * (cur/p.Area_n)/(p.a_n*p.L_n)/i_0n;
  xp=-0.5 * (cur/p.Area_p)/(p.a_p*p.L_p)/i_0p;
 
-% Overpotentials
- RTaF=(p.R*TEMP)./(p.alph*p.Faraday);
+% Overpotentials [V]
+
+RTaF=(p.R*TEMP)./(p.alph*p.Faraday);
  
-%  eta_n  = RTaF .* asinh(cur ./ (2.*p.a_n.*p.Area_n.*p.L_n.*i_0n));
-%  eta_p  = RTaF .* asinh(-cur ./ (2.*p.a_p.*p.Area_p.*p.L_p.*i_0p));
+eta_n  = RTaF .* asinh(cur ./ (2.*p.a_n.*p.Area_n.*p.L_n.*i_0n));
+eta_p  = RTaF .* asinh(-cur ./ (2.*p.a_p.*p.Area_p.*p.L_p.*i_0p));
 
-eta_p=(2*p.R*TEMP)/p.Faraday * log(xp + sqrt(1+xp*xp));
-eta_n=(2*p.R*TEMP)/p.Faraday * log(xn + sqrt(1+xn*xn));
+% eta_p=(2*p.R*TEMP)/p.Faraday * log(xp + sqrt(1+xp*xp));
+% eta_n=(2*p.R*TEMP)/p.Faraday * log(xn + sqrt(1+xn*xn));
 
-% SPM Voltage
+% SPM Voltage [V]
  V_spm= eta_p - eta_n + V_ocv;
 
 
 %% Degredation  
 
 
-Sn=3*p.eps_s_n*p.L_n*p.Area_n/p.R_n;  %m^2                                         % from prada
+Sn=3*p.eps_s_n*p.L_n*p.Area_n/p.R_n;  %m^2                                      
 
-it=cur/Sn;                            %A/m^2                                       % from prada
+% Overpotential of the SEI [V]
 
-% Overpotential of the SEI
+eta_sei_n= Uref_n + eta_n - p.Us + (sei*p.Rsei_n)*cur;       %2037.4 R                        
 
-eta_sei_n= Uref_n + eta_n - p.Us + (sei*p.Rsei_n)*cur;       %2037.4 R                          % from howey
-
-% Current density of the SEI
+% Current density of the SEI [I m-2]
 
 ksei = p.ksei* exp((130000/p.R)*(1/p.T_ref - 1/TEMP)); 
-Js= real (p.Faraday*ksei*exp(-p.Faraday/(p.R*TEMP) *( eta_sei_n) ));                           % from howey
+is= real (p.Faraday*ksei*exp(-p.Faraday/(p.R*TEMP) *( eta_sei_n) ));                        
 
-% Growth rate of SEI layer
+% Growth rate of SEI layer [m]
 
-sei_dot = Js/(p.Faraday*p.rhos);
+sei_dot = is/(p.Faraday*p.rhos);
 
 % Total resistance (film + growing SEI layer)
 
@@ -293,32 +276,30 @@ R_tot_n = p.Rsei_n*sei + p.Rcell;
 
 % cyclable Li, evolution of the charge 
 
-Q_dot= -Sn*Js;                                                                    % from prada
+Q_dot= -Sn*is;                                                                    
 
-% calculation of negative side concentration after adding SEI effect
+% Total molar flux [mol m-2 s-1]
 
-js_n = (J_n) + (Js/p.Faraday);
+js_n = (J_n) + (is/p.Faraday);
 
-% Solid particle concentration 
+% Solid particle concentration [mol m-3] 
 
 c_p = A_p*U_p + B_p.*J_p;
 c_n = A_n*U_n + B_n.*js_n;  
 
 %% Heat generation  
-% Qohmic =  -cur.*(V_spm - V_ocv);
+
 Qohmic =  cur.^2*R_tot_n;
-Qreaction=   cur.*(eta_n - eta_p) ;             
-Qentropic= 0;       %cur*TEMP*(dUpdT-dUndT)./1000  %cur*TEMP*(dudT)./1000
 Qgen= Qohmic + 0 + 0;
-
+  
 % Heat remove
-Qremv= p.h*p.A*(T-298.15);
+Qremv= p.h*p.A*(T-p.T_cool);  
 
- % Temperature calculation
- T1_dot= Qgen./p.Cc + (T2-T1)./(p.Rc*p.Cc); %Tc core temp
- T2_dot= (p.T_amb-T2)./(p.Ru*p.Cs) - (T2-T1)./(p.Rc*p.Cs); %Ts surface tem. 
+% Temperature calculation [K]
+T1_dot= Qgen./p.Cc + (T2-T1)./(p.Rc*p.Cc); %Tc core temp
+T2_dot= (p.T_amb-T2)./(p.Ru*p.Cs) - (T2-T1)./(p.Rc*p.Cs); %Ts surface tem. 
 
- % Lumped Temperature calculation
+% Lumped Temperature calculation [K]
 T_dot= (Qgen  - Qremv)./(p.M*p.Cp);
 
 %% Outputs
@@ -328,22 +309,9 @@ varargout{1} = theta_p;
 varargout{2} = theta_n;
 varargout{3} = V_spm;
 varargout{4} = V_ocv;
-varargout{5} = p.Ds_n;
-varargout{6} = p.Ds_p;
-varargout{7} = p.k_n;
-varargout{8} = p.k_p;
-varargout{9} = sn;
-varargout{10}= sp;
-varargout{11}= eta_sei_n;
-varargout{12}= Qohmic;
-varargout{13}= Qreaction;
-varargout{14}= Qremv;
-varargout{15}= T_dot;
-varargout{16}= Q_dot;
-varargout{17}= sei_dot;
-varargout{18}= R_tot_n;
-varargout{19}= BAh;
-varargout{20}= cur;
+varargout{5} = Uref_n;
+varargout{6} = Uref_p;
+varargout{7}= cur;
 end
 
 
@@ -371,52 +339,39 @@ cur=-data.Current(t);
 TEMP=T;
 %% Solid phase dynamics
 
-% Molar flux for solid phase
+% Molar flux for solid phase [mol m-2 s-1]
 J_p=-(cur./p.Area_p)./(p.Faraday*p.a_p*p.L_p);
 J_n=(cur./p.Area_n)./(p.Faraday*p.a_n*p.L_n);
 
-% Solid phase diffusivity temperature dependence
+% Solid phase diffusivity temperature dependence [m s-1]
 p.Ds_n = p.Ds_n0 * exp(p.E.Dsn/p.R*(1/p.T_ref - 1/TEMP));
 p.Ds_p = p.Ds_p0 * exp(p.E.Dsp/p.R*(1/p.T_ref - 1/TEMP)) ;
 
 % Matrices for solid-phase Li concentration
  [A_p,A_n,B_n,B_p]= matrixs(p);
 
-% Calculation of the surface concentration
-
+% Calculation of the surface concentration [mol m-3]
+ 
 c_ss_p= U_p(end);
 c_ss_n= U_n(end);
  
 
 %% Calculation of potential of the cell
 
-% SOC of the electrodes  
+% li-fraction  of the electrodes 
  [theta_p,theta_n]=getsoc(c_ss_p,c_ss_n,p);
  
-%% li-fraction
-AMp= (p.eps_s_p*p.L_p*p.Area_p);
-AMn= (p.eps_s_n*p.L_n*p.Area_n);
-BAh = cur*t/3600;
-sp = -cur * t / (p.Faraday) / (p.eps_s_p*p.L_p*p.Area_p) / p.c_s_p_max;
-sn = -cur * t / (p.Faraday) / (p.eps_s_n*p.L_n*p.Area_n) / p.c_s_n_max;
- 
-%% cell OCV
-% cell_Ah=OCVcell(:,1);
-% cell_OCV=OCVcell(:,2);
-% V_ocv= interp1(cell_Ah,cell_OCV,Ah,'linear'); % Ah charge sırasında eksi oluyor!!! 
+% OCV of the cell [V]
 
-%% OCV 
  [Uref_p, Uref_n]=refpotantial (theta_p, theta_n,KokamOCVNMC, KokamNMC, KokamOCVC, KokamC, OCVcell);
+ V_ocv = Uref_p-Uref_n  ;
 
-% OCV of the cell
-  V_ocv = Uref_p-Uref_n  ;
-
-% Kinetic reaction rate, adjusted for Arrhenius temperature dependence
+% Kinetic reaction rate, adjusted for Arrhenius temperature dependence [(A/m^2)*(mol^3/mol)^(1+alpha)]
 
  p.k_n = p.k_n0 * exp(p.E.kn/p.R*(1/p.T_ref - 1/TEMP)); 
  p.k_p = p.k_p0 * exp(p.E.kp/p.R*(1/p.T_ref - 1/TEMP)); 
 
-% Exchange current density
+% Exchange current density [I m-2]
 
  i_0n = p.k_n * p.Faraday * sqrt(((p.c_s_n_max - c_ss_n) .* c_ss_n .* p.ce));
  i_0p = p.k_p * p.Faraday * sqrt(((p.c_s_p_max - c_ss_p) .* c_ss_p .* p.ce));
@@ -424,74 +379,68 @@ sn = -cur * t / (p.Faraday) / (p.eps_s_n*p.L_n*p.Area_n) / p.c_s_n_max;
  xn= 0.5 * (cur/p.Area_n)/(p.a_n*p.L_n)/i_0n;
  xp=-0.5 * (cur/p.Area_p)/(p.a_p*p.L_p)/i_0p;
 
-% Overpotentials
+% Overpotentials [V]
  RTaF=(p.R*TEMP)./(p.alph*p.Faraday);
  
-%  eta_n  = RTaF .* asinh(cur ./ (2.*p.a_n.*p.Area_n.*p.L_n.*i_0n));
-%  eta_p  = RTaF .* asinh(-cur ./ (2.*p.a_p.*p.Area_p.*p.L_p.*i_0p));
+ eta_n  = RTaF .* asinh(cur ./ (2.*p.a_n.*p.Area_n.*p.L_n.*i_0n));
+ eta_p  = RTaF .* asinh(-cur ./ (2.*p.a_p.*p.Area_p.*p.L_p.*i_0p));
 
-  eta_p=(2*p.R*TEMP)/p.Faraday * log(xp + sqrt(1+xp*xp));
-  eta_n=(2*p.R*TEMP)/p.Faraday * log(xn + sqrt(1+xn*xn));
+%  eta_p=(2*p.R*TEMP)/p.Faraday * log(xp + sqrt(1+xp*xp));
+%  eta_n=(2*p.R*TEMP)/p.Faraday * log(xn + sqrt(1+xn*xn));
 
-% SPM Voltage
+% SPM Voltage [V]
  V_spm= eta_p - eta_n + V_ocv;
 
 
 %% Degredation  
 
 
-Sn=3*p.eps_s_n*p.L_n*p.Area_n/p.R_n;  %m^2                                         % from prada
+Sn=3*p.eps_s_n*p.L_n*p.Area_n/p.R_n;  %m^2                                                                         
 
-it=cur/Sn;                            %A/m^2                                       % from prada
+% Overpotential of the SEI [V]
 
-% Overpotential of the SEI
+eta_sei_n= Uref_n + eta_n - p.Us + (sei*p.Rsei_n )*cur;                                 
 
-eta_sei_n= Uref_n + eta_n - p.Us + (sei*p.Rsei_n )*cur;                                 % from howey
-
-% Current density of the SEI
+% Current density of the SEI [I m-2]
 
 ksei = p.ksei* exp((130000/p.R)*(1/p.T_ref - 1/TEMP)); 
-Js= real (p.Faraday*ksei*exp(-p.Faraday/(p.R*TEMP) *( eta_sei_n) ));                           % from howey
+is= real (p.Faraday*ksei*exp(-p.Faraday/(p.R*TEMP) *( eta_sei_n) ));                            
 
-% Growth rate of SEI layer
+% Growth rate of SEI layer [m]
 
-sei_dot = Js/(p.Faraday*p.rhos);
+sei_dot = is/(p.Faraday*p.rhos);
 
-% Total resistance (film + growing SEI layer)
+% Total resistance (film + growing SEI layer) 
 
 R_tot_n = p.Rsei_n*sei + p.Rcell;
 
 % cyclable Li, evolution of the charge 
 
-Q_dot= -Sn*Js;                                                                    % from prada
+Q_dot= -Sn*is;                                                                  
 
-% calculation of negative side concentration after adding SEI effect
+% Total molar flux [mol m-2 s-1]
 
-js_n = (J_n) + (Js/p.Faraday);
+js_n = (J_n) + (is/p.Faraday);
 
-% Solid particle concentration 
+% Solid particle concentration [mol m-3]
 
 c_p = A_p*U_p + B_p.*J_p;
 c_n = A_n*U_n + B_n.*js_n;  
 
 
 %% Heat generation  
-% Qohmic =  -cur.*(V_spm - V_ocv);
+
 Qohmic =  cur.^2*R_tot_n;
-Qreaction=   cur.*(eta_n - eta_p) ;             
-Qentropic= 0;       %cur*TEMP*(dUpdT-dUndT)./1000  %cur*TEMP*(dudT)./1000
 Qgen= Qohmic + 0 + 0;
-
+  
 % Heat remove
+Qremv= p.h*p.A*(T-p.T_cool);  
 
-Qremv= p.h*p.A*(T-298.15);
+% Temperature calculation [K]
+T1_dot= Qgen./p.Cc + (T2-T1)./(p.Rc*p.Cc); %Tc core temp
+T2_dot= (p.T_amb-T2)./(p.Ru*p.Cs) - (T2-T1)./(p.Rc*p.Cs); %Ts surface tem. 
 
- % Temperature calculation
- T1_dot= Qgen./p.Cc + (T2-T1)./(p.Rc*p.Cc); %Tc core temp
- T2_dot= (p.T_amb-T2)./(p.Ru*p.Cs) - (T2-T1)./(p.Rc*p.Cs); %Ts surface tem. 
-
- % Lumped Temperature calculation
-
+% Lumped Temperature calculation [K]
 T_dot= (Qgen  - Qremv)./(p.M*p.Cp);
 
 %% Outputs
@@ -501,22 +450,9 @@ varargout{1} = theta_p;
 varargout{2} = theta_n;
 varargout{3} = V_spm;
 varargout{4} = V_ocv;
-varargout{5} = p.Ds_n;
-varargout{6} = p.Ds_p;
-varargout{7} = p.k_n;
-varargout{8} = p.k_p;
-varargout{9} = sn;
-varargout{10}= sp;
-varargout{11}= eta_sei_n;
-varargout{12}= Qohmic;
-varargout{13}= Qreaction;
-varargout{14}= Qremv;
-varargout{15}= T_dot;
-varargout{16}= Q_dot;
-varargout{17}= sei_dot;
-varargout{18}= R_tot_n;
-varargout{19}= BAh;
-varargout{20}= cur;
+varargout{5} = Uref_n;
+varargout{6} = Uref_p;
+varargout{7}= cur;
 end
 
 
